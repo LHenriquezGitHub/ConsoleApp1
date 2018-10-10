c66200467c2b1f6ed0550099fddb5f9f136ebe90
LHenriquezGitHub <magicspider@gmail.com>
2018-10-10 11:37:42 -0400

    Added Util Helper Class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,17 +11,22 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            int[,] matrix = new int[,]
-            {{ 4, 2, 4, 8},
-             { 3, 8, 8, 4},
-             { 4, 2, 4, 8},
-             { 4, 2, 4, 4}};
+            //int[,] matrix = new int[,]
+            //{{ 4, 2, 4, 8},
+            // { 3, 8, 8, 4},
+            // { 4, 2, 4, 8},
+            // { 4, 2, 4, 4}};
 
-            var tracker = (int)Math.Sqrt(matrix.Length);
-            Console.WriteLine(Math.Abs( SubstractDiagonalValues(matrix, tracker, tracker)));
+            //var tracker = (int)Math.Sqrt(matrix.Length);
+            //Console.WriteLine(Math.Abs(SubstractDiagonalValues(matrix, tracker, tracker)));
+            //Console.WriteLine(SubstractDiagonalValues(matrix));
 
+            //int number = 5;
+            //Console.WriteLine($"Factorial of: { number } is { Factorial(number) }.");
+            //Console.WriteLine($"Fibonacci of: { number } is { Fibonacci(number) }.");          
 
-            Console.WriteLine(WriteLeftToRightSum(matrix));
+            Util.SayHello();
+
             Console.WriteLine("Press Enter to Exit");
             Console.ReadLine();
         }
@@ -33,10 +38,10 @@ namespace ConsoleApp1
             var sum = matrix[index, index] - matrix[index, (tracker - 1) - index];
 
             return sum + SubstractDiagonalValues(matrix, index, tracker);
-            
+
         }
 
-        public static int WriteLeftToRightSum(int[,] ar)
+        public static int SubstractDiagonalValues(int[,] ar)
         {
             var leftSum = 0;
             var rightSum = 0;
@@ -50,6 +55,18 @@ namespace ConsoleApp1
             }
 
             return Math.Abs(leftSum - rightSum);
+        }
+
+
+        public static int Factorial(int number)
+        {
+            if (number == 0 || number == 1) { return 1; }
+            return number * Factorial(number - 1);
+        }
+        public static int Fibonacci(int number)
+        {
+            if (number == 0 || number == 1) { return 1; }
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
         }
     }
 }
---
+++ ConsoleApp1/Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static class Util
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine($"Hello!");
+        }
+    }
+}
